51f3d05b921683d1a5f7b8caecf48f74995f9005
Nikita C <cethrivias@gmail.com>
2023-10-15 14:28:34 +0200

    nvim-tmux navigation

---
+++ lua/custom/plugins/vim-tmux-navigator.lua
@@ -0,0 +1,6 @@
+-- 1 - update
+-- 2 - wall
+-- More: https://github.com/christoomey/vim-tmux-navigator#autosave-on-leave
+vim.g.tmux_navigator_save_on_switch = 2
+
+return { 'christoomey/vim-tmux-navigator' }
