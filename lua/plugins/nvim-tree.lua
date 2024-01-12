c6911d41b2c4011050a05aa04e457018d3d932f7
cethris <cethrivias@gmail.com>
2024-01-12 21:25:34 +0100

    nvim-tree diagnostics

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -4,7 +4,12 @@ return {
     lazy = false,
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
-        require('nvim-tree').setup()
+        require('nvim-tree').setup {
+            diagnostics = {
+                enable = true,
+                show_on_dirs = true,
+            },
+        }
         vim.keymap.set('n', '<leader>1', '<cmd>NvimTreeToggle<cr>', {})
         vim.keymap.set('n', '<leader>!', '<cmd>NvimTreeFindFile<cr>', {})
     end,
